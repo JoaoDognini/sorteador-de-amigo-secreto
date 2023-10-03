a5e123e049d1c0cc91c681a56d147344dbc38e68
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2023-10-03 13:57:51 -0300

    Aula 1 - Bem começado, metade feito!
    Iniciando a criação dos testes e cumprindo desafio de estilizar o cabeçalho do formulário.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cabecalho from './componentes/Cabecalho';
+
+function App() {
+	return (
+    <>
+		<Cabecalho />
+	</>
+  );
+}
+
+export default App;
---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,18 @@
+// Jest
+
+import { render, screen } from "@testing-library/react"
+import Formulario from "./Formulario";
+
+test('Quando input estiver vazio, novos participantes não podem ser adicionados', () => {
+
+	render(<Formulario />)
+
+	//Encontrar no DOM o input
+	const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+	//Encontrar o botão
+	const botao = screen.getByRole('button');
+	//Garantir que o input esteja no documento
+	expect(input).toBeInTheDocument();
+	//Garantir que o botão esteja desabilitado
+	expect(botao).toBeDisabled(); 
+})
